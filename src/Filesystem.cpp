d7e9bad14e34ca05b6bbb60e03b87bf0735effa2
Kili <kili+git@mailbox.org>
2018-06-23 21:34:41 +0200

    fix wrong used smart pointer

--- src/Filesystem.cpp
+++ src/Filesystem.cpp
@@ -59,9 +59,10 @@ unsigned int Filesystem::charToInt(char *buffer) {
 	return static_cast<int>(buffer[0]) << 24 | buffer[1] << 16 | buffer[2] << 8 | buffer[3];
 }
 
-void Filesystem::calcCRC32(char* in, char* out) {
-	CRC32 c3;
-	strcpy(out, c3(in).c_str());
+void Filesystem::calcCRC32(char* buffer, size_t bufferLength, char crc32Bytes[CRC32::HashBytes]) {
+	CRC32 crc = CRC32();
+	crc.add(buffer, bufferLength);
+	crc.getHash((unsigned char *)crc32Bytes);
 	return;
 }
 
@@ -95,23 +96,21 @@ void FilesystemClient::compareFiles(const string FID, shared_ptr<File> f) {
 		char *buffer = new char[partLength];
 		char *tmpcrc = new char[4];
 		bool *n = new bool;
-		while (readFile(FID, buffer, i,n) != 0){
-			calcCRC32(buffer, tmpcrc);
+		int bufferLength = -1;
+		while ((bufferLength = readFile(FID, buffer, i,n)) > 0){
+			calcCRC32(buffer, bufferLength, tmpcrc);
 			if ((f->crcMap.find(i) != f->crcMap.end()))	 {
 				cout << f->crcMap[i] << endl;
 				if (strcmp(tmpcrc, f->crcMap[i].get()->data())!= 0) {
 					Logger::debug("Found File Delta: " + FID + ": " + to_string(i));
 					f->np->addPart(i);
-					shared_ptr<array<char,4>> t = make_shared<array<char,4>>();
-					strcpy(t.get()->data(), tmpcrc);
-					f->crcMap[i] = t;
+					strcpy(f->crcMap[i].get()->data(), tmpcrc);				
 				}
 			}else {
 				f->np->addPart(i);
-				// Logger::debug("Added file part: "+ FID + ": " + to_string(i));
-				shared_ptr<array<char,4>> t = make_shared<array<char,4>>();
-				strcpy(t.get()->data(), tmpcrc);
-				f->crcMap.insert(make_pair(i, t));
+				Logger::debug("Added file part: "+ FID + ": " + to_string(i));
+				f->crcMap[i] = make_shared<array<char,4>>();
+				strcpy(f->crcMap[i].get()->data(), tmpcrc);
 			}
 			i++;
 		}
